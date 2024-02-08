62cea6160595db3c2820073e1be61968d1de0a47
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2024-02-08 17:22:45 +0000

    Create main.py

---
+++ projeto1/main.py
@@ -0,0 +1,64 @@
+from random import *
+
+lista = []
+N = 0
+K = 0
+
+def gera():
+    global K, lista
+    lista = []
+    
+    K = randint(1, 2 * N)
+
+    for i in range(N):
+        lista.append(randint(1,N))
+
+def algoritmo1():
+    for i in range(N):
+        for j in range(i,N):
+            if lista[i]==lista[j]:
+                continue
+
+            if lista[i]+lista[j] == K:
+                print("True")
+                return
+    print("False")
+
+def algoritmo2():
+    lista.sort()
+    ini = 0
+    fim = N-1
+    while ini != fim:
+        if lista[ini] == lista[fim]:
+            print("False")
+            return
+
+        soma = lista[ini]+lista[fim]
+        if soma < K:
+            ++ini
+        if soma > K:
+            --fim
+        if soma == K:
+            print("True")
+            return
+        
+    print("False")
+
+def algoritmo3():
+
+    check = [0]*N
+
+    print("False")
+
+def main():
+    global N
+
+    for i in range(1,6):
+        N = i*20000
+        gera()
+
+    algoritmo1()
+    algoritmo3()
+    algoritmo2()
+
+main()
