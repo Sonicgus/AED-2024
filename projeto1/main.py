33a08be1f6db372c360a2e16d1d12d6dff556386
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2024-02-15 12:31:27 +0000

    Update main.py

--- projeto1/main.py
+++ projeto1/main.py
@@ -1,34 +1,37 @@
 from random import *
+from time import time
 
-lista = []
-N = 0
-K = 0
 
 def gera():
     global K, lista
     lista = []
-    
+
     K = randint(1, 2 * N)
 
     for i in range(N):
-        lista.append(randint(1,N))
+        lista.append(randint(1, N))
+
 
 def algoritmo1():
     for i in range(N):
-        for j in range(i,N):
-            if lista[i]==lista[j]:
+        for j in range(i, N):
+            if lista[i] == lista[j]:
                 continue
 
-            if lista[i]+lista[j] == K:
-                print("True")
-                return
-    print("False")
+            if lista[i] + lista[j] == K:
+                encontrado = True
+
+    if encontrado:
+        print("True")
+    else:
+        print("False")
+
 
 def algoritmo2():
     lista.sort()
 
     ini = 0
-    fim = N-1
+    fim = N - 1
 
     while ini != fim and lista[ini] != lista[fim]:
         soma = lista[ini] + lista[fim]
@@ -37,34 +40,50 @@ def algoritmo2():
             print("True")
             return
         if soma < K:
-            ini+=1
+            ini += 1
         elif soma > K:
-            fim-=1
-        
+            fim -= 1
+
     print("False")
+
 
 def algoritmo3():
-
-    check = [0]*2*N
+    check = [0] * 2 * N
 
     for elemento in lista:
-        if check[K-elemento]:
+        if check[K - elemento]:
             print("True")
             return
-        check[elemento]=1
-    
+        check[elemento] = 1
+
     print("False")
 
-def main():
-    global N
 
-    for i in range(1,6):
-        N = i*20000
+def main():
+    global N, K, lista
+
+    for i in range(1, 6):
+        N = i * 20000
         gera()
 
+        t1ini = time()
         algoritmo1()
+        t1fim = time()
+
+        t3ini = time()
         algoritmo3()
+        t3fim = time()
+
+        t2ini = time()
         algoritmo2()
+        t2fim = time()
+
+        print("\\/\\/\\/", N, "\\/\\/\\/")
+        print(t1fim - t1ini)
+        print(t2fim - t2ini)
+        print(t3fim - t3ini)
+        print("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\")
+
 
 if __name__ == "__main__":
     main()
